2d3e4f70c670b7bc766bb44fef8f887b261da8cd
Joaquin Gomez <joaquingomez@my.whccd.edu>
2025-01-15 19:47:37 -0800

    Add project files.

---
+++ COMP003A.LectureActivity1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Import namespace
+namespace COMP003A.LectureActivity1
+{
+    // Class declaration
+    public class Program
+    {
+        // Entry point of the program
+        public static void Main(string[] args)
+        {
+            //Code to execute
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
